4ad9427201598457c0f1d288a1216a47862810ea
ron-ligsay <akyligsay16@gmail.com>
2023-07-21 12:51:28 +0800

    initial index reactjs

---
+++ index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Frame = () => {
+    return (
+        <div className="frame">
+            <div></div>
+            <div></div>
+        </div>
+    )
+}

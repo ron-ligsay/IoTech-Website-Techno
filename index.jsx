159258990f775daeef21b3e98ceba3eaafac30a4
ron-ligsay <akyligsay16@gmail.com>
2023-07-21 13:07:55 +0800

    nav bar

--- index.jsx
+++ index.jsx
@@ -4,6 +4,26 @@ import React from "react";
 export const Frame = () => {
   return (
     <div className="frame">
+      <div className="frame">
+      <div className="div">
+        <div className="navbar">
+          <img className="rectangle" alt="Rectangle" src="rectangle-33.png" />
+          <div className="text-wrapper">Products</div>
+          <div className="text-wrapper-2">Services</div>
+          <div className="text-wrapper-3">About</div>
+          <div className="text-wrapper-4">Contact</div>
+        </div>
+        <div className="overlap">
+          <img className="img" alt="Rectangle" src="rectangle-34.png" />
+          <p className="p">Total control with the rebuilt IoTech website.</p>
+        </div>
+        </div>
+        <h1 className="stay-connected-be">
+          Stay Connected,
+          <br />
+          Be Connected.
+        </h1>
+        </div>
       <div className="div-2">
         <div className="rectangle-2" />
         <div className="overlap">

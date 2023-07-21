200279a8ceb2c6c1e286e94ba06548233771d8e6
ron-ligsay <akyligsay16@gmail.com>
2023-07-21 12:54:02 +0800

    added sections

--- index.jsx
+++ index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
+
 export const Frame = () => {
-    return (
-        <div className="frame">
-            <div></div>
-            <div></div>
+  return (
+    <div className="frame">
+      <div className="div-2">
+        <div className="rectangle-2" />
+        <div className="overlap">
         </div>
-    )
-}
+        <div className="overlap-2">
+          
+        </div>
+        <p className="p">Your Helpful Home Starts Here</p>
+        <div className="overlap-4">
+          
+        </div>   
+        </div>
+    </div>
+  );
+};

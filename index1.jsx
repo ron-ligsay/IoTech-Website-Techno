6fb7aed1709d43cbd3105eb53535b88be223e0ac
ron-ligsay <akyligsay16@gmail.com>
2023-07-21 13:12:34 +0800

    new design index

---
+++ index1.jsx
@@ -0,0 +1,289 @@
+import React from "react";
+import { Group } from "./Group";
+import "./style1.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div-2">
+        <div className="rectangle-2" />
+        <div className="overlap">
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="rectangle-5" />
+          <div className="rectangle-6" />
+        </div>
+        <div className="overlap-2">
+          <img className="rectangle-7" alt="Rectangle" src="rectangle-35.png" />
+          <img className="line" alt="Line" src="line-1.svg" />
+          <div className="text-wrapper-2">HOME</div>
+          <div className="text-wrapper-3">SHOW NOW</div>
+          <div className="text-wrapper-4">SHOP</div>
+          <div className="text-wrapper-5">ABOUT</div>
+          <div className="text-wrapper-6">CONTACT</div>
+          <div className="text-wrapper-7">$0.00</div>
+          <img className="white-logo" alt="White logo" src="white-logo-1.png" />
+          <div className="rectangle-8" />
+          <div className="overlap-group-wrapper">
+            <div className="overlap-group-2">
+              <img className="polygon" alt="Polygon" src="polygon-1.svg" />
+              <div className="ellipse" />
+            </div>
+          </div>
+          <div className="overlap-wrapper">
+            <div className="overlap-3">
+              <img className="rectangle-9" alt="Rectangle" src="rectangle-37.svg" />
+              <div className="ellipse-2" />
+              <div className="ellipse-3" />
+              <div className="ellipse-4" />
+              <div className="text-wrapper-8">0</div>
+            </div>
+          </div>
+          <h1 className="be-connected-right">
+            Be Connected,
+            <br />
+            Right Now!
+          </h1>
+          <div className="text-wrapper-9">WELCOME TO IOTECH STORE</div>
+          <div className="rectangle-10" />
+        </div>
+        <p className="p">Your Helpful Home Starts Here</p>
+        <div className="overlap-4">
+          <div className="text-wrapper-10">SHOW NOW</div>
+          <div className="rectangle-11" />
+        </div>
+        <p className="text-wrapper-11">Your Helpful Home Starts Here</p>
+        <p className="text-wrapper-12">Organize your favorites in a single tab.</p>
+        <p className="text-wrapper-13">Seamless automations are a tap away.</p>
+        <p className="text-wrapper-14">Check in on your home with just a tap.</p>
+        <p className="text-wrapper-15">
+          Bring your devices together for more personalized help around the home. And create automations tailored to
+          your needs to simplify everyday tasks.
+        </p>
+        <div className="rectangle-12" />
+        <div className="rectangle-13" />
+        <div className="rectangle-14" />
+        <img className="line-2" alt="Line" src="line-2.svg" />
+        <div className="overlap-5">
+          <div className="text-wrapper-10">LEARN MORE &gt;</div>
+          <div className="rectangle-11" />
+        </div>
+        <div className="text-wrapper-16">Book a professional installation.</div>
+        <p className="text-wrapper-17">
+          We offer professional devices installation. Easily purchase your Nest devices with installation and schedule
+          online any day of the week.
+        </p>
+        <img className="line-3" alt="Line" src="line-3.svg" />
+        <div className="group-2">
+          <div className="overlap-6">
+            <div className="text-wrapper-18">Door Lock</div>
+            <div className="text-wrapper-19">Door Lock</div>
+          </div>
+          <div className="text-wrapper-20">$29.90 - $34.90</div>
+          <div className="group-3">
+            <img className="star-5" alt="Star" src="star-1-3.svg" />
+            <img className="star-6" alt="Star" src="star-2-4.svg" />
+            <img className="star-7" alt="Star" src="star-3-4.svg" />
+            <img className="star-8" alt="Star" src="star-4-4.svg" />
+            <img className="star-9" alt="Star" src="star-5-4.svg" />
+          </div>
+          <div className="rectangle-15" />
+        </div>
+        <div className="overlap-7">
+          <Group className="group-8" />
+          <div className="rectangle-16" />
+        </div>
+        <div className="overlap-8">
+          <Group
+            className="group-8"
+            img="star-2-14.svg"
+            star="star-1-13.svg"
+            star1="star-3-14.svg"
+            star2="star-4-14.svg"
+            star3="star-5-14.svg"
+          />
+          <div className="rectangle-16" />
+        </div>
+        <div className="overlap-9">
+          <Group
+            className="group-8"
+            img="star-2-15.svg"
+            star="star-1-14.svg"
+            star1="star-3-15.svg"
+            star2="star-4-15.svg"
+            star3="star-5-15.svg"
+          />
+          <div className="rectangle-16" />
+        </div>
+        <div className="group-4">
+          <div className="overlap-6">
+            <div className="text-wrapper-18">Door Lock</div>
+            <div className="text-wrapper-19">Door Lock</div>
+          </div>
+          <div className="text-wrapper-20">$29.90 - $34.90</div>
+          <div className="group-3">
+            <img className="star-5" alt="Star" src="star-1-5.svg" />
+            <img className="star-6" alt="Star" src="star-2-6.svg" />
+            <img className="star-7" alt="Star" src="star-3-6.svg" />
+            <img className="star-8" alt="Star" src="star-4-6.svg" />
+            <img className="star-9" alt="Star" src="star-5-6.svg" />
+          </div>
+          <div className="rectangle-15" />
+        </div>
+        <div className="overlap-10">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-21">Door Lock</div>
+        </div>
+        <div className="text-wrapper-22">$29.90 - $34.90</div>
+        <div className="group-5">
+          <img className="star-10" alt="Star" src="star-1-6.svg" />
+          <img className="star-11" alt="Star" src="star-2-7.svg" />
+          <img className="star-12" alt="Star" src="star-3-7.svg" />
+          <img className="star-13" alt="Star" src="star-4-7.svg" />
+          <img className="star-14" alt="Star" src="star-5-7.svg" />
+        </div>
+        <div className="overlap-11">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-21">Door Lock</div>
+        </div>
+        <div className="text-wrapper-23">$29.90 - $34.90</div>
+        <div className="group-6">
+          <img className="star-10" alt="Star" src="star-1-7.svg" />
+          <img className="star-11" alt="Star" src="star-2-8.svg" />
+          <img className="star-12" alt="Star" src="star-3-8.svg" />
+          <img className="star-13" alt="Star" src="star-4-8.svg" />
+          <img className="star-14" alt="Star" src="star-5-8.svg" />
+        </div>
+        <div className="overlap-12">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-21">Door Lock</div>
+        </div>
+        <div className="text-wrapper-24">$29.90 - $34.90</div>
+        <div className="group-7">
+          <img className="star-10" alt="Star" src="star-1-8.svg" />
+          <img className="star-11" alt="Star" src="star-2-9.svg" />
+          <img className="star-12" alt="Star" src="star-3-9.svg" />
+          <img className="star-13" alt="Star" src="star-4-9.svg" />
+          <img className="star-14" alt="Star" src="star-5-9.svg" />
+        </div>
+        <img className="rectangle-17" alt="Rectangle" src="rectangle-34.png" />
+        <img className="pz" alt="Pz" src="pz-0ek-1ko.png" />
+        <img className="sh" alt="Sh" src="sh-1-3xnb-2.png" />
+        <img className="element-aare-ya" alt="Element" src="8aare-3ya.png" />
+        <p className="text-wrapper-25">Most Love by the Customers</p>
+        <p className="text-wrapper-26">Most Love by the Customers</p>
+        <div className="text-wrapper-27">Follow Us @Internetofthingstech</div>
+        <div className="overlap-13">
+          <img className="rectangle-18" alt="Rectangle" src="rectangle-48.png" />
+          <div className="rectangle-8" />
+          <div className="text-wrapper-28">WATCH THE VIDEOS</div>
+          <p className="text-wrapper-29">All your favorites in one place.</p>
+          <div className="rectangle-19" />
+        </div>
+        <img className="line-4" alt="Line" src="line-4.svg" />
+        <img className="line-5" alt="Line" src="line-5.svg" />
+        <img className="line-6" alt="Line" src="line-6.svg" />
+        <div className="overlap-14">
+          <div className="text-wrapper-30">HOME</div>
+          <div className="text-wrapper-31">SHOP</div>
+        </div>
+        <div className="overlap-15">
+          <div className="text-wrapper-30">ABOUT</div>
+          <div className="text-wrapper-32">CONTACT</div>
+        </div>
+        <p className="text-wrapper-33">Mauris lacus nunc pellentesque eget bibendum vitae scelerisque</p>
+        <p className="text-wrapper-34">
+          Sed odio donec curabitur auctor amet tincidunt non odio enim felis tincidunt amet morbi egestas hendrerit.
+        </p>
+        <p className="text-wrapper-35">
+          Sed odio donec curabitur auctor amet tincidunt non odio enim felis tincidunt amet morbi egestas hendrerit.
+        </p>
+        <p className="text-wrapper-36">
+          Sed odio donec curabitur auctor amet tincidunt non odio enim felis tincidunt amet morbi egestas hendrerit.
+        </p>
+        <div className="text-wrapper-37">JENNIFER LEWIS</div>
+        <div className="text-wrapper-38">ALICIA HEART</div>
+        <div className="text-wrapper-39">JUAN CARLOS</div>
+        <div className="ellipse-5" />
+        <div className="ellipse-6" />
+        <div className="ellipse-7" />
+        <div className="group-9">
+          <img className="star-15" alt="Star" src="star-1.svg" />
+          <img className="star-16" alt="Star" src="star-2.svg" />
+          <img className="star-17" alt="Star" src="star-3.svg" />
+          <img className="star-18" alt="Star" src="star-4.svg" />
+          <img className="star-19" alt="Star" src="star-5.svg" />
+        </div>
+        <div className="group-10">
+          <img className="star-15" alt="Star" src="image.svg" />
+          <img className="star-16" alt="Star" src="star-2-2.svg" />
+          <img className="star-17" alt="Star" src="star-3-2.svg" />
+          <img className="star-18" alt="Star" src="star-4-2.svg" />
+          <img className="star-19" alt="Star" src="star-5-2.svg" />
+        </div>
+        <div className="group-11">
+          <img className="star-15" alt="Star" src="star-1-2.svg" />
+          <img className="star-16" alt="Star" src="star-2-3.svg" />
+          <img className="star-17" alt="Star" src="star-3-3.svg" />
+          <img className="star-18" alt="Star" src="star-4-3.svg" />
+          <img className="star-19" alt="Star" src="star-5-3.svg" />
+        </div>
+        <p className="text-wrapper-40">© 2023 IoTech Products and Service. Powered by IoTech Products and Service.</p>
+        <div className="group-12">
+          <div className="overlap-6">
+            <div className="text-wrapper-18">Door Lock</div>
+            <div className="text-wrapper-19">Door Lock</div>
+          </div>
+          <div className="text-wrapper-20">$29.90 - $34.90</div>
+          <div className="group-3">
+            <img className="star-5" alt="Star" src="star-1-9.svg" />
+            <img className="star-6" alt="Star" src="star-2-10.svg" />
+            <img className="star-7" alt="Star" src="star-3-10.svg" />
+            <img className="star-8" alt="Star" src="star-4-10.svg" />
+            <img className="star-9" alt="Star" src="star-5-10.svg" />
+          </div>
+          <div className="rectangle-15" />
+        </div>
+        <div className="overlap-16">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-19">Door Lock</div>
+        </div>
+        <div className="text-wrapper-41">$29.90 - $34.90</div>
+        <div className="group-13">
+          <img className="star-5" alt="Star" src="star-1-10.svg" />
+          <img className="star-6" alt="Star" src="star-2-11.svg" />
+          <img className="star-7" alt="Star" src="star-3-11.svg" />
+          <img className="star-8" alt="Star" src="star-4-11.svg" />
+          <img className="star-9" alt="Star" src="star-5-11.svg" />
+        </div>
+        <div className="rectangle-20" />
+        <div className="overlap-17">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-19">Door Lock</div>
+        </div>
+        <div className="text-wrapper-42">$29.90 - $34.90</div>
+        <div className="group-14">
+          <img className="star-5" alt="Star" src="star-1-11.svg" />
+          <img className="star-6" alt="Star" src="star-2-12.svg" />
+          <img className="star-7" alt="Star" src="star-3-12.svg" />
+          <img className="star-8" alt="Star" src="star-4-12.svg" />
+          <img className="star-9" alt="Star" src="star-5-12.svg" />
+        </div>
+        <div className="rectangle-21" />
+        <div className="overlap-18">
+          <div className="text-wrapper-18">Door Lock</div>
+          <div className="text-wrapper-19">Door Lock</div>
+        </div>
+        <div className="text-wrapper-43">$29.90 - $34.90</div>
+        <div className="group-15">
+          <img className="star-5" alt="Star" src="star-1-12.svg" />
+          <img className="star-6" alt="Star" src="star-2-13.svg" />
+          <img className="star-7" alt="Star" src="star-3-13.svg" />
+          <img className="star-8" alt="Star" src="star-4-13.svg" />
+          <img className="star-9" alt="Star" src="star-5-13.svg" />
+        </div>
+        <div className="rectangle-22" />
+      </div>
+    </div>
+  );
+};
